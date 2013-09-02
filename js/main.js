bf31d7c19954eca4de29709e176f9533ba2a0b4f
Sergey Bespalov <sergey.bespalov@chisw.us>
2013-09-02 17:15:03 +0300

    re-factoring code, create init method and core parameters

--- js/main.js
+++ js/main.js
@@ -1,76 +1,106 @@
-var $fields_coords = {};
-var $coord_map = {};
-var $alfa = {
-    1: 'a',
-    2: 'b',
-    3: 'c',
-    4: 'd',
-    5: 'e',
-    6: 'f',
-    7: 'g',
-    8: 'h',
-};
+var boardClass = function() {
 
-for(var i=1; i<9; i++) {
-    $coord_map[i] = {};
-    for(var k=1; k<9; k++) {
-        $fields_coords[$alfa[i]+k] = {
-            x: 43+35*(i-1), y: 43+35*(k-1)
-        };
-        $coord_map[i][k] = $alfa[i]+k;
+    var $fields_coords = {},
+        $coord_map= {},
+        $field= null,
+        $board= null,
+        $stage= null,
+        $currentFigure= null,
+        $figures= null,
+        $fields= null,
+        $board_ofset= null,
+        figure_types= {},
+        $alfa = {}
+    ;
+
+    // Функция доступная только внутри класса.
+    function figure_move(figure) {
+        console.log('figure: ', figure);
+//            return true;
     }
-}
-//console.log('$fields_coords: ', $fields_coords);
 
-$(document).ready(function() {
-    var boardClass = function(elem) {
-        var $field;
-        var $stage = Math.floor((parseInt($(elem).css('width')))/10);
+    function mouse_move($e) {
 
-        var $board = $(elem),
-                $currentFigure  = null,
-                $figures        = $(".figure"),
-                $fields         = $('.field'),
-                $board_ofset    = $(elem).offset()
-                ;
+        var $mouseX = Math.floor($e.clientX - 10 -  $board_ofset.left);
+        var $mouseY = Math.floor($e.clientY - 10 - $board_ofset.top);
 
-        $board.on("mousemove", function($e) {
-            if ($board.$currentFigure) {
-                var $mouseX = Math.floor($e.clientX - 10 - $board_ofset.left);
-                var $mouseY = Math.floor($e.clientY - 10 - $board_ofset.top);
+        if ($mouseX > $stage && $mouseY > $stage && $mouseX < $stage * 8 + 16 && $mouseY < $stage * 8 + 16) {
+            $field = $coord_map[Math.round(($mouseX - 10) / $stage)][Math.round(($mouseY - 10) / $stage)];
 
-                if($mouseX > $stage && $mouseY > $stage && $mouseX < $stage*8+16 && $mouseY < $stage*8+16) {
-                    field = $coord_map[Math.round(($mouseX-10) / $stage)][Math.round(($mouseY-10) / $stage)];
-                
 //                    $('#mouseX').val(Math.round(($mouseX-10) / $stage));
 //                    $('#mouseY').val(Math.round(($mouseY-10) / $stage));
-                    $('#FIELD').val(field);
-                    $board.$currentFigure.css({
-                        'left': $e.clientX - 10 - $board_ofset.left,
-                        'top': $e.clientY - 10 - $board_ofset.top,
-                        'z-index': 10
-                    });
+            $('#FIELD').val($field);
+            $board.$currentFigure.css({
+                'left': $e.clientX - 10 - $board_ofset.left,
+                'top': $e.clientY - 10 - $board_ofset.top,
+                'z-index': 10
+            });
+        }
+    }
+
+    return {
+        init: function() {
+
+            $board= $('#chessDesck'),
+            $stage= Math.floor((parseInt($($board).css('width'))) / 10),
+            $figures= $(".figure"),
+            $fields= $('.field'),
+            $board_ofset= $($board).offset(),
+            figure_types= {
+                warior: {
+                    first_step: 2,
+                    step: 1,
+                    direction: 'F',
+                    strike: 'angle',
+                    move_back: false
+                }
+            },
+            $alfa = {
+                1: 'a',
+                2: 'b',
+                3: 'c',
+                4: 'd',
+                5: 'e',
+                6: 'f',
+                7: 'g',
+                8: 'h',
+            };
+            for (var i = 1; i < 9; i++) {
+                $coord_map[i] = {};
+                for (var k = 1; k < 9; k++) {
+                    $fields_coords[$alfa[i] + k] = {
+                        x: 43 + 35 * (i - 1), y: 43 + 35 * (k - 1)
+                    };
+                    $coord_map[i][k] = $alfa[i] + k;
                 }
             }
-        });
 
-        $figures.on("mousedown", function() {
-            $board.$currentFigure = $(this);
-        });
+            $board.on("mousemove", function($e) {
+                if ($board.$currentFigure) {
+                    mouse_move($e);
+                }
+            });
 
-        $board.on("mouseup", function($e) {
-            if ($board.$currentFigure) {
-                $board.$currentFigure.css({
-                    'left': $fields_coords[field].x,
-                    'top': $fields_coords[field].y,
-                    'z-index': 1
-                });
-            }
-            
-            $board.$currentFigure = null;
-        });
+            $figures.on("mousedown", function() {
+                figure_move(this);
+                $board.$currentFigure = $(this);
+            });
 
-    };
+            $board.on("mouseup", function($e) {
+                if ($board.$currentFigure && $field) {
+                    $board.$currentFigure.css({
+                        'left': $fields_coords[$field].x,
+                        'top': $fields_coords[$field].y,
+                        'z-index': 1
+                    });
+                }
 
-    board = new boardClass("#chessDesck");
-});
+                $board.$currentFigure = null;
+            });
+        },
+//        move: function(figure) {
+//
+//        }
+    }
+
+};
